2b315bb9105419c05769f1689a96d64f6b20b6ed
pagansxc <paganseacrest@gmail.com>
2020-12-12 18:37:06 +0300

    final student 2 debug

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ const { styles } = require("./gulp_tasks/styles");
 const { serv } = require("./gulp_tasks/serv");
 const { watch } = require("./gulp_tasks/watch");
 const {image} = require("./gulp_tasks/img")
+const {fonts} = require("./gulp_tasks/fonts-icons")
 const {del} = require("./gulp_tasks/del")
 
-exports.dev = parallel(serv, watch, series(styles, image, scripts));
-exports.build = series(del, styles, image, scripts);
+exports.dev = parallel(serv, watch, series(styles, image, fonts, scripts));
+exports.build = series(del, styles, image, fonts, scripts);
